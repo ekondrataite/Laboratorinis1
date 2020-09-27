329becb61b9e08bb80a4dccd5767b89793c1d224
ekondrataite <71488211+ekondrataite@users.noreply.github.com>
2020-09-27 16:46:58 +0300

    Add files via upload

--- pirmaVektoriai.cpp
+++ pirmaVektoriai.cpp
@@ -16,12 +16,32 @@ struct studentas {
 
 int main()
 {
-    studentas eilute;
-    cout << "Iveskite varda ir pavarde: ";
-    cin >> eilute.vardas >> eilute.pavarde;
+    vector<studentas> grupe;
+    studentas laikinas;
+    int n = 0;
 
-    float n;
-    cout << "Iveskite semestro pazymius: ";
-    while (cin >> n)
-        eilute.pazymiai.push_back(n);
+    cout << "Iveskite studentu, kuriu duomenis pildysite, skaiciu: ";
+    cin >> n;
+    grupe.reserve(n);
+
+    vector<int> pazymiai;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Iveskite studento varda, pavarde, egzamino ivertinima: " << endl;
+        cin >> laikinas.vardas >> laikinas.pavarde >> laikinas.egzaminas;
+
+        grupe.push_back(laikinas);
+
+        cout << "Iveskite studento semestro namu darbu pazymius" << endl;
+        cout << "Suvede visus pazymius, suveskite 'end' ir spauskite Enter" << endl;
+        
+    }
+
+    cout << "Ivesti duomenys" << endl;
+    cout << "--------------------------" << endl;
+    for (auto &g : grupe) {
+        cout << g.vardas << " " << g.pavarde << " " << g.egzaminas << endl;
+    }
+    grupe.clear();
+
 }
