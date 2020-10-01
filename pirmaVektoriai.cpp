2d51a8b147a152297f321920032613c3f1298936
ekondrataite <71488211+ekondrataite@users.noreply.github.com>
2020-10-01 09:08:42 +0300

    Add files via upload

--- pirmaVektoriai.cpp
+++ pirmaVektoriai.cpp
@@ -6,13 +6,16 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <fstream>
+#include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 
 struct studentas {
 	string vardas;
 	string pavarde;
-	std::vector<int> pazymiai;
+	vector<int> pazymiai;
 	float egzaminas;
 	float galutinis;
 };
@@ -42,52 +45,114 @@ float galutinis_balas(float vidurkis, int egzaminas) {
 	return 0.4 * vidurkis + 0.6 * egzaminas;
 }
 
+bool compareV(studentas a, studentas b) {
+	return a.vardas < b.vardas;
+}
+bool compareP(studentas a, studentas  b) { 
+	return a.pavarde < b.pavarde;
+}
+
 int main()
 {
+	cout << "Ar norite duomenis ivesti ar nuskaityti is failo?" << endl;
+	cout << "Iveskite 'Nuskaityti', jei duomenis nuskaitysite is failo" << endl;
+	cout << "Iveskite 'Ivesti', jei ranka ivesite duomenis" << endl;
+
+	string pasirinkimas;
+	cin >> pasirinkimas;
+
 	vector<studentas> grupe;
 	studentas laikinas;
-	int n = 0;
+	
+	//failo nuskaitymas
 
-	cout << "Iveskite studentu, kuriu duomenis pildysite, skaiciu: ";
-	cin >> n;
+	if (pasirinkimas == "Nuskaityti") {
+		ifstream file("studentai10000.txt");
 
-	grupe.reserve(n);
+		string vardas, pavarde;
+		int pazymiai;
+		float egzaminas;
+		int l;
 
-	for (int i = 0; i < n; i++) {
-		cout << "Iveskite studento varda, pavarde, egzamino ivertinima: " << endl;
-		cin >> laikinas.vardas >> laikinas.pavarde >> laikinas.egzaminas;
+		file >> l;
 
-		cout << "Iveskite studento semestro namu darbu pazymius" << endl;
-		cout << "Suvede visus pazymius, suveskite 'end' ir spauskite Enter" << endl;
+		for (int key = 0; key < l; key++) {
+			file >> vardas;
+			grupe[key].vardas;
 
-		laikinas.pazymiai.reserve(50);
-		string input = "";
-		while (input != "end") {
+			file >> pavarde;
+			grupe[key].pavarde;
 
-			cin >> input;
-			if (input == "end") {
-				break;
+			file >> pazymiai;
+			laikinas.pazymiai.push_back(key);
+
+			file >> egzaminas;
+			grupe[key].egzaminas;
+		}
+		return l;
+
+		
+	}
+
+	if (pasirinkimas == "Ivesti") {
+		int n = 0;
+
+		cout << "Iveskite studentu, kuriu duomenis pildysite, skaiciu: ";
+		cin >> n;
+
+		grupe.reserve(n);
+
+		for (int i = 0; i < n; i++) {
+			cout << "Iveskite studento varda, pavarde, egzamino ivertinima: " << endl;
+			cin >> laikinas.vardas >> laikinas.pavarde >> laikinas.egzaminas;
+
+			cout << "Iveskite studento semestro namu darbu pazymius" << endl;
+			cout << "Suvede visus pazymius, suveskite 'end' ir spauskite Enter" << endl;
+
+			laikinas.pazymiai.reserve(50);
+			string input = "";
+			while (input != "end") {
+
+				cin >> input;
+				if (input == "end") {
+					break;
+				}
+				laikinas.pazymiai.push_back(stoi(input));
 			}
-			laikinas.pazymiai.push_back(std::stoi(input));
-		}
-		grupe.push_back(laikinas);
+			grupe.push_back(laikinas);
 
-		cout <<"Studentas: " << laikinas.vardas << " " << laikinas.pavarde << endl;
-		cout << "Egzamino ivertinimas: " << laikinas.egzaminas << endl;
-		cout << "Semestro namu darbu pazymiai: ";
-		for (auto& p : laikinas.pazymiai) {
-			cout << p << " ";
+			cout << "Studentas: " << laikinas.vardas << " " << laikinas.pavarde << endl;
+			cout << "Egzamino ivertinimas: " << laikinas.egzaminas << endl;
+			cout << "Semestro namu darbu pazymiai: ";
+			for (auto& p : laikinas.pazymiai) {
+				cout << p << " ";
+			}
+			cout << endl;
 		}
-		cout << endl;
+	}
+
+	cout << "Iveskite kaip norite rusiuoti duomenis" << endl;
+	cout << "Jei rusiuosite pagal vardus iveskite 'V'" << endl;
+	cout << "Jei rusiuosite pagal pavardes iveskite 'P'" << endl;
+
+	string p;
+	cin >> p;
+
+	if (p == "V") {
+		sort(grupe.begin(), grupe.end(), compareV);
+	}
+	else {
+		sort(grupe.begin(), grupe.end(), compareP);
 	}
 
 	cout << "Iseitis" << endl;
-	cout << "--------------------------" << endl;
+	cout << "Vardas" << setw(30) << "Pavarde" << setw(30) << "Galutinis balas (vidurkis)" << setw(30);
+	cout << "Galutinis balas (mediana)" << endl;
+	cout << "------------------------------------------------------------------------------------------" << endl;
 	for (auto& g : grupe) {		
-		cout << g.vardas << " " << g.pavarde << " Gal.Vid: " << galutinis_balas(vidurkis(g.pazymiai), g.egzaminas);
-		cout << " Gal.Med: " << galutinis_balas(mediana(g.pazymiai), g.egzaminas) << endl;
+		cout << g.vardas << setw(30) << g.pavarde << setw(30) << galutinis_balas(vidurkis(g.pazymiai), g.egzaminas) << setw(30);
+		cout << setw(30) << galutinis_balas(mediana(g.pazymiai), g.egzaminas) << endl;
 		g.pazymiai.clear();
 	}
 	grupe.clear();
-
 }
